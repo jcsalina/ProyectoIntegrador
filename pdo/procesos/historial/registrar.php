64068433ae015727f3d77f5cb72008bff3fc2a2c
jcsalina <juansalinas_e@hotmail.com>
2019-02-06 22:50:19 -0500

    QQQ

--- pdo/procesos/historial/registrar.php
+++ pdo/procesos/historial/registrar.php
@@ -56,7 +56,8 @@ if (isset($_POST['submit_historial'])) {
     $pentavalente1  = isset($_POST["pentavalente1"]) ? $fecha_now : NULL;
     $pentavalente2  = isset($_POST["pentavalente2"]) ? $fecha_now : NULL;
     $pentavalente3  = isset($_POST["pentavalente3"]) ? $fecha_now : NULL;
-    $poliomielitis1 = isset($_POST["poliomielitis1"]) ? $fecha_now : NULL;
+    //$poliomielitis1 = isset($_POST["poliomielitis1"]) ? $fecha_now : NULL;
+    $poliomielitis1 = $_POST["poliomielitis1"]=="0000-00-00" ? $fecha_now : NULL;
     $poliomielitis2 = isset($_POST["poliomielitis2"]) ? $fecha_now : NULL;
     $poliomielitis3 = isset($_POST["poliomielitis3"]) ? $fecha_now : NULL;
     $neumococo1     = isset($_POST["neumococo1"]) ? $fecha_now : NULL;
